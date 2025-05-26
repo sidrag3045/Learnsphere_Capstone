8c8a70030f3d2e9f291743e7ca72e395767020b9
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-26 23:24:25 +0530

    Fixed File path of main.jsx in index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '..public/vite.svg'
-import './index.css'
-
 function App() {
   return <h1>Hello from LearnSphere ✨</h1>;
 }
