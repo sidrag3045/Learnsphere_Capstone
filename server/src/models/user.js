8488d0466cf2261be71d720bbd8ca730d233e0c7
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-30 20:23:49 +0530

    initialising user.js file with sequelize-cli and making model

---
+++ server/src/models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // we can define association here
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    fullName: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM('student', 'instructor'),
+      defaultValue: 'student'
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true
+  });
+  return User;
+};
