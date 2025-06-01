7212f55c34b1540e87b46b29f26739bc51feb2c2
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-01 20:07:33 +0530

    Removed unnecessary gitkeep files and older hashpasswords file

--- server/src/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-// Requiring bcrypt module
-const bcrypt = require('bcrypt');
-
-const hashPassword = async (plainPassword) => {
-  const saltRounds = 10;
-  return await bcrypt.hash(plainPassword, saltRounds);
-};
-
-module.exports = hashPassword;
