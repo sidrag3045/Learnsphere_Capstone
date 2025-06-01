2de20550240aa1ad522b8caa788ad5bfabe54e42
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-01 20:06:47 +0530

    Mouting auth router, and mounting cookie parser middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 // Initializing Express app
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Health check route (Base route)
 app.get('/', (req, res) => {
