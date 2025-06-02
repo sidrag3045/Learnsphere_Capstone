76ae6861ffe764eef47712d94b99890160f7885e
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-02 16:58:03 +0530

    Fixed some errors regarding mismatched model definition and migrations setup

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -17,21 +15,31 @@ module.exports = (sequelize, DataTypes) => {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
+      primaryKey: true,
+      allowNull: false
     },
-    fullName: DataTypes.STRING,
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      allowNull: false
     },
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     role: {
       type: DataTypes.ENUM('student', 'instructor'),
-      defaultValue: 'student'
+      defaultValue: 'student',
+      allowNull: false
     }
   }, {
     sequelize,
