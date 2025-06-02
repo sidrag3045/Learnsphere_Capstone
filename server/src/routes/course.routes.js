98342b9a29ebcf4e72ce93f1c55be9f9871cc086
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-02 21:27:16 +0530

    Made routes for Course API

---
+++ server/src/routes/course.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const { createCourse, 
+        getAllCourses, 
+        getCourseById, 
+        updateCourse, 
+        deleteCourse } = require('../controllers/courseController');
+
+const { verifyJWT, authorizeRoles } = require('../middlewares/authMiddleware');
+
+// Course routes
+
+// POST /api/courses - Create a new course
+router.post(
+  '/',
+  verifyJWT,
+  authorizeRoles('instructor'),
+  createCourse
+);
+
+// GET /api/courses - Get all courses
+router.get('/', getAllCourses);
+
+// GET /api/courses/:id - Get a course by ID
+router.get('/:id', getCourseById);
+
+// PUT /api/courses/:id - Update a course by ID
+router.put(
+  '/:id',
+  verifyJWT,
+  authorizeRoles('instructor'),
+  updateCourse
+);
+
+// DELETE /api/courses/:id - Delete a course by ID
+router.delete(
+  '/:id',
+  verifyJWT,
+  authorizeRoles('instructor'),
+  deleteCourse
+);
+
+module.exports = router;
