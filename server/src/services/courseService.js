8b2f325bf08bb443c0dadf41a85f45bb12863438
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-02 21:28:49 +0530

    Separated Controller logic further into Course Service for modularity

---
+++ server/src/services/courseService.js
@@ -0,0 +1,43 @@
+const { Course, User } = require('../models');
+
+const createCourseService = async (data, instructorId) => {
+  return await Course.create({ ...data, createdBy: instructorId });
+};
+
+const getAllCoursesService = async () => {
+  return await Course.findAll({
+    include: {
+      model: User,
+      as: 'instructor',
+      attributes: ['id', 'fullName', 'email']
+    },
+    order: [['createdAt', 'DESC']]
+  });
+};
+
+const getCourseByIdService = async (id) => {
+  return await Course.findByPk(id, {
+    include: {
+      model: User,
+      as: 'instructor',
+      attributes: ['id', 'fullName', 'email']
+    }
+  });
+};
+
+const updateCourseService = async (id, updatedData) => {
+  return await Course.update(updatedData, { where: { id } });
+};
+
+const deleteCourseService = async (id) => {
+  return await Course.destroy({ where: { id } });
+};
+
+
+module.exports = {
+  createCourseService,
+  getAllCoursesService,
+  getCourseByIdService,
+  updateCourseService,
+  deleteCourseService
+};
