4b5466678f576153077eecbf6f48e4827b2e1ff2
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-02 21:26:57 +0530

    Added associations for User having Courses

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // we can define association here
+      User.hasMany(models.Course, {
+        foreignKey: 'createdBy',
+        as: 'courses'
+      });
     }
   }
   User.init({
