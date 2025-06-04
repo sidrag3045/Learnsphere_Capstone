eb13cb0d94e8c36e0a133f283583375f09a79a70
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-04 16:04:15 +0530

    Fixing bugs in modules model

---
+++ server/src/models/module.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Module extends Model {
+    static associate(models) {
+      Module.belongsTo(models.Course, {
+        foreignKey: 'courseId',
+        as: 'course'
+      });
+
+      // Future: Module.hasMany(models.Lesson, ...)
+    }
+  }
+
+  Module.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    courseId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
+  }, {
+    sequelize,
+    modelName: 'Module',
+    tableName: 'modules',
+    timestamps: true
+  });
+
+  return Module;
+};
