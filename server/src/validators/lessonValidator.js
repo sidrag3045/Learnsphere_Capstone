0d40146b7135682409315adc6b3b57a8b2813b9e
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-05 18:32:09 +0530

    Created validations for Lesson schema

---
+++ server/src/validators/lessonValidator.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const lessonSchema = Joi.object({
+  title: Joi.string().min(3).max(255).required(),
+  description: Joi.string().min(10).max(1000).optional(),
+  videoUrl: Joi.string().uri().required(),
+  duration: Joi.number().integer().required(),
+  order: Joi.number().integer().required(),
+  status: Joi.string().valid('draft', 'published', 'archived').required(),
+  moduleId: Joi.string().guid({ version: ['uuidv4'] }).required()
+});
+
+const reorderLessonsSchema = Joi.object({
+  lessons: Joi.array().items(
+    Joi.object({
+      id: Joi.string().uuid().required(),
+      order: Joi.number().integer().required()
+    })
+  ).min(1).required()
+});
+
+module.exports = {
+  lessonSchema,
+  reorderLessonsSchema
+};
