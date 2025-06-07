797f09316cc720aab579b1d8316970808b970efd
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-07 16:35:28 +0530

    Service to generate signedUrl for uploading content to aws bucket

--- server/src/services/aws/s3Service.js
+++ server/src/services/aws/s3Service.js
@@ -1,4 +1,4 @@
-const { GetObjectCommand } = require('@aws-sdk/client-s3');
+const { GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const s3 = require('./s3Client');
 const AWS_CONFIG = require('../../config/aws');
@@ -18,24 +18,24 @@ const generateSignedUrl = async (s3Key, expiresIn = 900) => {
   }
 };
 
-// Upload file from buffer (if needed later in backend)
-// const uploadFileToS3 = async (s3Key, fileBuffer, mimeType) => {
-//   try {
-//     const command = new PutObjectCommand({
-//       Bucket: AWS_CONFIG.bucketName,
-//       Key: s3Key,
-//       Body: fileBuffer,
-//       ContentType: mimeType
-//     });
+// Generate a signed URL for uploading (video/PDF)
+const generateUploadSignedUrl = async (s3Key, contentType) => {
+  try {
+    const command = new PutObjectCommand({
+      Bucket: AWS_CONFIG.bucketName,
+      Key: s3Key,
+      ContentType: contentType
+    });
 
-//     await s3.send(command);
-//     return `s3://${AWS_CONFIG.bucketName}/${s3Key}`;
-//   } catch (error) {
-//     console.error('Error uploading to S3:', error);
-//     throw new Error('Upload failed');
-//   }
-// };
+    return await getSignedUrl(s3, command, { expiresIn: 900 });
+  } catch (error) {
+    console.error('Error generating upload URL:', error);
+    throw new Error('Could not generate upload URL');
+  }
+};
+
 
 module.exports = {
-  generateSignedUrl
+  generateSignedUrl,
+  generateUploadSignedUrl
 };
