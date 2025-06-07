24b8252441de511a9c362a885a33c61e913af877
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-08 01:03:27 +0530

    AWS Config file for leading env variables

--- server/src/config/aws.js
+++ server/src/config/aws.js
@@ -4,7 +4,8 @@ const AWS_CONFIG = {
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: process.env.AWS_REGION,
-  bucketName: process.env.AWS_BUCKET_NAME
+  bucketName: process.env.AWS_BUCKET_NAME,
+  linkExpiry: process.env.AWS_S3_SIGNED_URL_EXPIRY
 };
 
 module.exports = AWS_CONFIG;
