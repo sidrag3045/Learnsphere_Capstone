8820a9c59925012b7862eb3ada06f76a0b2ba283
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-09 17:50:06 +0530

    Minor change in linkExpiry from string to int, and adding cloudfront domain

--- server/src/config/aws.js
+++ server/src/config/aws.js
@@ -5,7 +5,8 @@ const AWS_CONFIG = {
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: process.env.AWS_REGION,
   bucketName: process.env.AWS_BUCKET_NAME,
-  linkExpiry: process.env.AWS_S3_SIGNED_URL_EXPIRY
+  linkExpiry: parseInt(process.env.AWS_S3_SIGNED_URL_EXPIRY || '3600'),
+  cloudFrontDomain: process.env.CLOUDFRONT_PUBLIC_URL,
 };
 
 module.exports = AWS_CONFIG;
