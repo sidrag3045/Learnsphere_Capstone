07781fd00fbb2fedd6fea4f70625bdea661fde2c
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-11 11:33:34 +0530

    Loading cloudfront env variables from .env in aws config

--- server/src/config/aws.js
+++ server/src/config/aws.js
@@ -7,6 +7,9 @@ const AWS_CONFIG = {
   bucketName: process.env.AWS_BUCKET_NAME,
   linkExpiry: parseInt(process.env.AWS_S3_SIGNED_URL_EXPIRY || '3600'),
   cloudFrontDomain: process.env.CLOUDFRONT_PUBLIC_URL,
+  cloudFrontPrivateKeyPath: process.env.CLOUDFRONT_PRIVATE_KEY_PATH,
+  cloudFrontKeyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
+  cloudFrontSignedUrlExpiresIn: parseInt(process.env.CLOUDFRONT_SIGNED_URL_EXPIRES_IN || '300')
 };
 
 module.exports = AWS_CONFIG;
