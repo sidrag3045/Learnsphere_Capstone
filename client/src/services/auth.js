f08e2b8deb2316c0ea11b8b599168bd0184605a8
Raghav Chanana <raghav.chanana7@gmail.com>
2025-06-13 00:59:25 +0530

    Authentication service for API calls to authenticate (register, login, get current user, logout)

---
+++ client/src/services/auth.js
@@ -0,0 +1,21 @@
+import axiosInstance from './axiosInstance';
+
+export const loginUser = async (payload) => {
+  const res = await axiosInstance.post('/auth/login', payload);
+  return res.data;
+};
+
+export const registerUser = async (payload) => {
+  const res = await axiosInstance.post('/auth/register', payload);
+  return res.data;
+};
+
+export const getCurrentUser = async () => {
+  const res = await axiosInstance.get('/auth/me');
+  return res.data;
+};
+
+export const logoutUser = async () => {
+  const res = await axiosInstance.post('/auth/logout');
+  return res.data;
+};
